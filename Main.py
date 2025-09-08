e0e256fef05769668720ed98d8c55042d34534eb
Pedro Delgado <166747454+PedroViniciusMD@users.noreply.github.com>
2025-09-08 13:19:53 +0000

    feat: adicionada função de soma

--- Main.py
+++ Main.py
@@ -1,4 +1,13 @@
 ## Atividade aula
 
 UserChoice = int(input("Digite a operação que você quer (1 - soma, 2 - multiplicação, 3 - subtração): "))
-print(UserChoice)
+Number1 = int(input("Digite o primeiro número que você quer manipular: "))
+Number2 = int(input("Digite o segundo numero que você quer manipular: "))
+## print(UserChoice)
+
+
+def somar (a,b):
+    return a + b
+
+if (UserChoice == 1):
+    print (f"soma = {somar(Number1, Number2)}")
